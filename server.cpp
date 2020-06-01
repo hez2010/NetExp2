a5771c0d43ace3fab979651426b5998585e663d1
Steven He <hez2010@outlook.com>
2020-06-01 22:02:45 +0800

    Add socket server

---
+++ server.cpp
@@ -0,0 +1,66 @@
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+#include <cstdio>
+#include <thread>
+
+int main(int argc, char **argv)
+{
+    int code;
+    auto server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (server == INVALID_SOCKET)
+    {
+        code = WSAGetLastError();
+        printf("Failed to create socket.\n");
+        return code;
+    }
+
+    sockaddr_in socket_addr;
+    socket_addr.sin_family = AF_INET;
+    socket_addr.sin_port = htons(23333);
+    socket_addr.sin_addr.S_un.S_addr = INADDR_ANY;
+
+    if (bind(server, reinterpret_cast<sockaddr *>(&socket_addr), sizeof(socket_addr) == SOCKET_ERROR))
+    {
+        code = WSAGetLastError();
+        printf("Failed to bind socket.\n");
+        return code;
+    }
+
+    if (listen(server, SOMAXCONN) == SOCKET_ERROR)
+    {
+        code = WSAGetLastError();
+        printf("Failed to start listen.\n");
+        return code;
+    }
+
+    while (true)
+    {
+        sockaddr_in client_addr;
+        int len = sizeof(client_addr);
+        auto client = accept(
+            server,
+            reinterpret_cast<sockaddr *>(&client_addr),
+            &len);
+        if (client == INVALID_SOCKET)
+        {
+            code = WSAGetLastError();
+            printf("Failed to accept client, code: %d.\n", code);
+            continue;
+        }
+
+        printf("Client accepted: %s\n", inet_ntoa(client_addr.sin_addr));
+        auto t = std::thread([client, client_addr]() {
+            char buffer[1024];
+            auto echo = "Message received.";
+            int len = recv(client, buffer, 1023, 0);
+            if (len > 0)
+            {
+                buffer[len] = 0;
+                printf("Message from client %s:\n%s", inet_ntoa(client_addr.sin_addr), buffer);
+                send(client, echo, strlen(echo), 0);
+            }
+            closesocket(client);
+        });
+        t.detach();
+    }
+}
